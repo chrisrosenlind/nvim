57aa296a72dfe6b118fbf67448fd4cf7d32cf62b
Christoffer Rosenlind <christoffer.rosenlind@gmail.com>
2026-09-12 07:45:45 +0200

    Refactor nvim cfg
    Revamp

--- init.lua
+++ init.lua
@@ -1,62 +1,25 @@
--- vim.g.mapleader = " "
-
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 vim.opt.expandtab = true
-vim.opt.signcolumn = "yes"
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 
-vim.lsp.config("lua_ls", {
-  settings = { Lua = { diagnostics = { globals = { "vim" } } } }
-})
+-- Vs code theme
+vim.o.termguicolors = true
+vim.o.background = 'dark'
+vim.cmd.colorscheme('vscode')
 
-vim.lsp.config("ts_ls", {
-  cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-  root_markers = { "package.json", "tsconfig.json" },
-})
+-- Save on Leader + w
+vim.keymap.set('n', '<leader>w', '<cmd>w<CR>', { desc = 'Save' })
 
-vim.lsp.enable({ "lua_ls", "ts_ls" })
+-- Go to file tree with Leader + e
+vim.keymap.set('n', '<leader>e', vim.cmd.Explore, { desc = 'File explorer' })
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  { "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    {
-    "gmr458/vscode_modern_theme.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("vscode_modern").setup({
-            cursorline = true,
-            transparent_background = false,
-            nvim_tree_darker = true,
-        })
-        vim.cmd.colorscheme("vscode_modern")
-    end,
-    },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        mapping = cmp.mapping.preset.insert({
-          ["<C-space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-        }),
-        sources = {
-          { name = "nvim_lsp" },
-        },
-      })
-  end
-  },
-})
+-- Prettier format
+vim.keymap.set('n', '<leader>f', function()
+  vim.cmd('silent !npx prettier --write %')
+  vim.cmd('edit!')
+end, { desc = 'Format with Prettier' })
